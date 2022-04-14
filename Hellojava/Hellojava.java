a767f7146df77ab777d3b4796f0d9c2c5d40a664
ChuongDe <chuongpntde160221@fpt.edu.vn>
2022-04-14 17:46:45 +0700

    1\

---
+++ Hellojava/Hellojava.java
@@ -0,0 +1,6 @@
+package Hellojava;
+public class Hellojava{
+    public static void main(String[]args){
+        System.out.println(313 + 122);
+    }
+}
---
+++ Hellojava/test.java
@@ -0,0 +1,10 @@
+package Hellojava;
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Chuong PHam");
+    }
+}
+    
+       
+   
+    
