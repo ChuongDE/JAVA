85fc55a400c2709baa822341af0ff1ffff918f59
ChuongDe <chuongpntde160221@fpt.edu.vn>
2022-04-14 18:54:22 +0700

    1

--- LearnJava/learn.java
+++ LearnJava/learn.java
@@ -4,13 +4,19 @@ import java.util.Scanner;
 public class learn {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        while(a <= b){
-            if(a % 3 == 0 && a % 5 == 0){
-                System.out.print(a + " ");
-            }
-            a++;
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
         }
+
+        int maxValue = arr[0];
+        for(int i = 1; i < n; i++){
+            if(arr[i] > maxValue[i]){
+                maxValue = arr[i];
+            }
+        }
+        System.out.print(maxValue);
     }
 }
